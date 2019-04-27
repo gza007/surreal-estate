d17bfff6848c50544dfbef51b94b762584f59783
Geevan Patel <geevan.peter.patel@gmail.com>
2019-04-27 23:45:53 +0100

    set-up complete, Navbar added but not completed

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/app.css';
+import NavBar from './navBar';
+
+const App = () => (
+  <div>
+    <NavBar />
+    <h1 className="app">SURREAL ESTATE</h1>;
+  </div>
+);
+
+export default App;
---
+++ src/components/navBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/navbar.css';
+
+const NavBar = () => (
+  <div className="navBar">
+  hello
+  </div>
+);
+
+export default NavBar;
