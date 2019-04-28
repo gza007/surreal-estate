cd2065eda65b7099fca354a3c355608be6164457
Geevan Patel <geevan.peter.patel@gmail.com>
2019-04-28 17:28:03 +0100

    NavBar and Styling Completed

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import '../styles/navbar.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSign } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSign);
 
 const NavBar = () => (
   <div className="navBar">
-  hello
+    <div className="icon">
+      <FontAwesomeIcon
+        className="icon-logo"
+        icon="sign"
+      />
+      Surreal Estate
+    </div>
+    <ul className="nav">
+      <li className="item">View Properties</li>
+      <li className="item">Add a Property</li>
+    </ul>
   </div>
 );
 
