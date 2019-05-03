d2462a0f189e4e46a9d8dfc9d6ad129fbc531264
Geevan Patel <geevan.peter.patel@gmail.com>
2019-05-04 00:41:28 +0100

    Browser Router Added to Index, Switch component added to App, addProperties and properties pages/components added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import '../styles/app.css';
 import NavBar from './navBar';
+import Properties from './properties';
+import AddProperty from './addProperty';
+
 
 const App = () => (
   <div>
     <NavBar />
+    <Switch>
+      <Route exact path="/" component={Properties} />
+      <Route exact path="/add-property" component={AddProperty} />
+    </Switch>
     <h1 className="app">SURREAL ESTATE</h1>;
   </div>
 );
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -3,6 +3,7 @@ import '../styles/navbar.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSign } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 library.add(faSign);
 
@@ -16,8 +17,8 @@ const NavBar = () => (
       Surreal Estate
     </div>
     <ul className="nav">
-      <li className="item">View Properties</li>
-      <li className="item">Add a Property</li>
+      <Link className="item" to="/">View Properties</Link>
+      <Link className="item" to="/add-property">Add a Property</Link>
     </ul>
   </div>
 );
